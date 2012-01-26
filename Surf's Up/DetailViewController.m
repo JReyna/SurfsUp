97ad9cf401eab1f83a58530ead65e7733cdce23b
James Reyna <james.r.reyna@us.army.mil>
2012-01-26 08:42:00 -0600

    customize UILabel

--- Surf's Up/DetailViewController.m
+++ Surf's Up/DetailViewController.m
@@ -30,6 +30,8 @@
     
     [[self view] setBackgroundColor:[UIColor
               colorWithPatternImage:[UIImage imageNamed:@"bg_sand"]]];
+    
+    [rentSwitch setOnTintColor:[UIColor colorWithRed:0 green:175.0/255.0 blue:176.0/255.0 alpha:1.0]];
 }
 
 
